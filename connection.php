4b5773c96d1cd5faf40f66d21b8c88860cc0d8c3
Denis <iltg407@gmail.com>
2018-07-03 15:17:21 +0200

    connection.php

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "35.199.156.222:3306";
+$username = "my_library";
+$password = "MyLibrary5%";
+$database = "my_library";
+
+$conn = mysqli_connect($servername, $username, $password, $database) or die ("Connessione non riuscita");
+
+?>
