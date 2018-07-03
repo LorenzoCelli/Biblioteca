a6ba3d6a735d20ff792d79eb52dfe832ed6be765
LorenzoCelli <celli.lorenzo@yahoo.it>
2018-07-03 14:25:48 +0200

    m

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function scroll_to_right(element_id) {
     var element = document.getElementById(element_id);
-    element.style.transform = "translate(-33.33%,0)";
+    element.style.transform = "translate(-66.66%,0)";
 }
 function scroll_to_center(element_id) {
     var element = document.getElementById(element_id);
-    element.style.transform = "translate(0,0)";
+    element.style.transform = "translate(-33.33%,0)";
 }
 function scroll_to_left(element_id) {
     var element = document.getElementById(element_id);
-    element.style.transform = "translate(33.33%,0)";
+    element.style.transform = "translate(0,0)";
 }
