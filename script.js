9b083059b1d006e161de92dcc135ffb2d609caa8
LorenzoCelli <celli.lorenzo@yahoo.it>
2018-07-03 15:01:52 +0200

    translateX

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function scroll_to_right(element_id) {
     var element = document.getElementById(element_id);
-    element.style.transform = "translate(-66.66%,0)";
+    element.style.transform = "translateX(-66.66%)";
 }
 function scroll_to_center(element_id) {
     var element = document.getElementById(element_id);
-    element.style.transform = "translate(-33.33%,0)";
+    element.style.transform = "translateX(-33.33%)";
 }
 function scroll_to_left(element_id) {
     var element = document.getElementById(element_id);
-    element.style.transform = "translate(0,0)";
+    element.style.transform = "translateX(0)";
+    console.log("ciao");
 }
