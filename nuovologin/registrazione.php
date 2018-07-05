65b9ad7ba0b52e988111e2214639fd6b94783452
LorenzoCelli <celli.lorenzo@yahoo.it>
2018-07-05 17:14:11 +0200

    pagina registrazione finita

--- nuovologin/registrazione.php
+++ nuovologin/registrazione.php
@@ -1,4 +1,8 @@
-<?php session_start();?>
+<?php session_start();
+$uname = $_POST["username"];
+$email = $_POST["email"];
+$pass = $_POST["password"];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,61 +14,63 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 <body>
-            <div class="container">
-                <h1>Registrazione</h1>
-                <form action="registrazione.php" method="post">
-                    <div class="input_container">
-                        <input class="login_field" type="email" placeholder="e-mail" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
-                        <div id="emailmsg" class="alert"><div class="point">!</div>Email già in uso!</div>
-                    </div>
-                    <div class="input_container">
-                        <input class="login_field" type="text" placeholder="username" name="username" required>
-                        <div id="usrmsg" class="alert"><div class="point">!</div>Username già in uso!</div>
-                    </div>
-                    <div class="input_container">
-                        <input class="login_field" type="password" placeholder="password" name="password" id="pass" onmouseout="check_pass()" onblur="check_pass()" required>
-                    </div>
-                    <div class="input_container">
-                        <input class="login_field" type="password" placeholder="conferma password" name="confpass" id="pass_check" onmouseout="check_pass()" onblur="check_pass()" required>
-                        <div id="pass_alarm" class="alert"><div class="point">!</div>Le password non coincidono!</div>
-                    </div>
-                    <div class="input_container">
-                        <input class="login_button" type="submit" value="registrati" id="register_button">
-                    </div>
-                </form>
-                <?php
-                $uname = $_POST["username"];
-                $email = $_POST["email"];
-                $pass = $_POST["password"];
-
-                include '../connection.php';
-
-                $usercheck = mysqli_query($conn, "SELECT * FROM utenti WHERE username = '$uname'")->num_rows;
-                $emailcheck = mysqli_query($conn, "SELECT * FROM utenti WHERE email = '$email'")->num_rows;
-
-                if ($usercheck == 0 && $emailcheck == 0) {
-
-                  $id = mysqli_query($conn, "SELECT * FROM utenti")->num_rows;
-                  $_SESSION['id_utente'] = $id;
-                  $sql = "INSERT INTO utenti
-                  (username,email,password)
-                  VALUES
-                  ('$uname','$email','$pass')";
-                  $results = mysqli_query($conn, $sql);
-
-                  if ($results) echo "<script>window.open('main/main.php','_self');</script>";
-                  else echo "<b style='color:red;'>Qualcosa è andato storto, riprova.</b><br>";
-
-                }else{
-                  if ($usercheck != 0) echo "<script>document.getElementById('usrmsg').style.display = 'block' </script>";
-                  if ($emailcheck != 0) echo "<script>document.getElementById('emailmsg').style.display = 'block' </script>";
-                }
-
-                mysqli_close($conn);
-                ?>
-                <a href="index.html">indietro</a>
+    <div class="container">
+        <h1>Registrazione</h1>
+        <form action="registrazione.php" method="post">
+            <div class="input_container">
+                <input id="email" value="<?php echo $email; ?>" class="login_field" type="email" placeholder="e-mail" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
+                <div id="emailmsg" class="alert"><div class="point">!</div>Email già in uso!</div>
             </div>
-        </div>
-    <script src="script.js"></script>
+            <div class="input_container">
+                <input id="usr" value="<?php echo $uname; ?>" class="login_field" type="text" placeholder="username" name="username" required>
+                <div id="usrmsg" class="alert"><div class="point">!</div>Username già in uso!</div>
+            </div>
+            <div class="input_container">
+                <input class="login_field" type="password" placeholder="password" name="password" id="pass" onkeyup="check_pass()" required>
+            </div>
+            <div class="input_container">
+                <input onfocus="first_focus=true;" class="login_field" type="password" placeholder="conferma password" name="confpass" id="pass_check" onkeyup="check_pass()" required>
+                <div id="pass_alarm" class="alert"><div class="point">!</div>Le password non coincidono!</div>
+            </div>
+            <div class="input_container">
+                <input class="login_button" type="submit" value="registrati" id="register_button">
+            </div>
+        </form>
+        <script src="script.js"></script>
+
+        <?php
+        include '../connection.php';
+
+        $usercheck = mysqli_query($conn, "SELECT * FROM utenti WHERE username = '$uname'")->num_rows;
+        $emailcheck = mysqli_query($conn, "SELECT * FROM utenti WHERE email = '$email'")->num_rows;
+
+        if ($usercheck == 0 && $emailcheck == 0) {
+
+          $id = mysqli_query($conn, "SELECT * FROM utenti")->num_rows;
+          $_SESSION['id_utente'] = $id;
+          $sql = "INSERT INTO utenti
+          (username,email,password)
+          VALUES
+          ('$uname','$email','$pass')";
+          $results = mysqli_query($conn, $sql);
+
+          if ($results) echo "<script>window.open('main/main.php','_self');</script>";
+          else echo "<b style='color:red;'>Qualcosa è andato storto, riprova.</b><br>";
+
+        }else{
+            echo '<script> var uname = "'.$uname.'" </script>';
+            echo '<script> var email = "'.$email.'" </script>';
+            if ($usercheck != 0){
+                echo " <script> wrong_user(); </script> ";
+            }
+            if ($emailcheck != 0){
+                echo " <script> wrong_email(); </script> ";
+            }
+        }
+
+        mysqli_close($conn);
+        ?>
+        <a href="index.html">indietro</a>
+    </div>
 </body>
 </html>
--- nuovologin/script.js
+++ nuovologin/script.js
@@ -1,16 +1,50 @@
-var pass_id = "pass";
-var check_id = "pass_check";
-var target = "pass_alarm";
-var btn = "register_button";
+var pass = document.getElementById("pass");
+var check = document.getElementById("pass_check");
+var target = document.getElementById("pass_alarm");
+var btn = document.getElementById("register_button");
+
+var email_field =  document.getElementById("email");
+var email_msg = document.getElementById("emailmsg");
+var usr_field = document.getElementById("usr");
+var usr_msg = document.getElementById("usrmsg");
+
+var first_focus = false;
+
+function had_first_focus() {
+    first_focus = true;
+}
 
 function check_pass(){
-    var pass = document.getElementById(pass_id).value;
-    var check = document.getElementById(check_id).value;
-    var c = pass!==check;
-    document.getElementById(btn).disabled = c;
-    if(c){
-        document.getElementById(target).style.display = "block";
+    console.log(first_focus);
+    var c = pass.value!==check.value;
+    btn.disabled = c;
+    if(c && first_focus){
+        target.style.display = "block";
     }else{
-        document.getElementById(target).style.display = "none";
+        target.style.display = "none";
+    }
+}
+
+//uname
+//email
+
+function wrong_user(){
+    usr_msg.style.display="block";
+    usr_field.onkeyup = function () {
+        if(this.value === uname){
+            usr_msg.style.display = "block";
+        }else{
+            usr_msg.style.display = "none";
+        }
+    }
+}
+function wrong_email(){
+    email_msg.style.display="block";
+    email_field.onkeyup = function () {
+        if(this.value === email){
+            email_msg.style.display = "block";
+        }else{
+            email_msg.style.display = "none";
+        }
     }
 }
