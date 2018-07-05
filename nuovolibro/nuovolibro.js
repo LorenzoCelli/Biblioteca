c00f5fecfaaf99735e9de0b6497058888b8381a3
lorenzocelli <celli.lorenzo@yahoo.it>
2018-07-05 10:08:04 +0200

    nuova pagina principale

--- nuovolibro/nuovolibro.js
+++ nuovolibro/nuovolibro.js
@@ -1,8 +1,28 @@
 function show_hide(element_id) {
-    element = document.getElementById(element_id);
-    if(element.style.width == "" || element.style.width == "0px"){
+    var element = document.getElementById(element_id);
+    if(element.style.width === "" || element.style.width === "0px"){
         element.style.width = "500px";
     }else{
         element.style.width = "0px";
+    }
+}
+function slide_left(element_id){
+    var element = document.getElementById(element_id);
+    if(!element.is_open){
+        element.style.transform = "translateX(-400px)";
+        element.is_open = true;
+    }else{
+        element.style.transform = "translateX(0)";
+        element.is_open = false;
+    }
+}
+function slide_right(element_id){
+    var element = document.getElementById(element_id);
+    if(!element.is_open){
+        element.style.transform = "translateX(0)";
+        element.is_open = true;
+    }else{
+        element.style.transform = "translateX(-300px)";
+        element.is_open = false;
     }
 }
