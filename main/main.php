7d0c35538f5ef99ec58905ecbf991314683b4a7f
Denis <iltg407@gmail.com>
2018-07-11 11:13:47 +0200

    aggiunto menu account a main.php

--- main/main.php
+++ main/main.php
@@ -33,9 +33,14 @@ Main container
         </div>
 
         <div style="border-radius: 0 0 10px 0; height: 50px; width:50px; position: absolute; top:0; left: 0; background-color: #f8f8f8; display: inline-block"><img onclick="slide_main_menu()" src="../imgs/menu.svg" style="height: 50px"></div>
-        <div class="account_container">
+        <a onclick="show_menu_account()"><div class="account_container">
           <p2 id="nome_utente"><?php echo $uname;?></p2>
           <div style="display: inline-block; height: 50px; width: 50px; overflow: hidden"><img src="../imgs/usericon.svg" alt="icona_utente_non_trovata" style="width:50px;height:50px;"></div>
+        </div></a>
+        <div id="menu_account">
+            <button class="account_button" disabled>Il tuo account</button>
+            <button class="account_button">Impostazioni</button>
+            <button style="border-bottom: none" class="account_button">Logout</button>
         </div>
 
         <h1 style="color: white; margin: 0; line-height: 30px"> La tua biblioteca. </h1>
