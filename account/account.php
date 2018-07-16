f0ebc9875c3fc604c2d88c9ace28a8c5b026f421
Denis <iltg407@gmail.com>
2018-07-16 12:46:37 +0200

    page account e possibilità di modificare avatar, avatars da disegnare

--- account/account.php
+++ account/account.php
@@ -1,5 +1,8 @@
 <?php session_start();
+include "../connection.php";
 $uname = $_SESSION['uname'];
+$id_utente = $_SESSION['id_utente'];
+include 'scriptusericon.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,19 +29,35 @@ Main container
         <a href=""><button class="main_menu_button">Tua sorella</button></a>
     </div><!--
  --><div class="content">
-
         <div style="border-radius: 0 10px 10px 0; height: 50px; width:50px; position: absolute; top:20px; left: 0; background-color: #f8f8f8; display: inline-block"><img onclick="slide_main_menu()" src="../imgs/menu.svg" style="height: 50px"></div>
         <a onclick="show_menu_account()"><div class="account_container">
           <p2 id="nome_utente"><?php echo $uname;?></p2>
-          <div style="display: inline-block; height: 50px; width: 50px; overflow: hidden"><img src="../imgs/usericon.svg" alt="icona_utente_non_trovata" style="width:50px;height:50px;"></div>
+          <div style="display: inline-block; height: 50px; width: 50px; overflow: hidden"><img id="small_icon" src="<?php echo $img; ?>" alt="icona_utente_non_trovata" style="width:50px;height:50px;"></div>
         </div></a>
         <div id="menu_account" style="background-color:white;">
             <p class="account_menu_header">Il tuo account</p>
             <a href=""><button class="account_button">Impostazioni</button></a>
             <button style="border-bottom: none;border-radius:0 0 10px 10px;" class="account_button">Logout</button>
         </div>
+
+        <div id="change_div"></div><br>
+        <img src="<?php echo $img; ?>" alt="" style="margin-bottom:10px;" id="big_icon"><br>
+        <button id="avatar_button" onclick="show_avatars();">Cambia avatar</button>
+        <div id="avatars" style="display:none;">
+          <h1>Scegli il tuo avatar:</h1>
+          <img src="../imgs/avatars/1.png" alt="1" class="scelta" onclick="change_avatar(this);">
+          <img src="../imgs/avatars/2.png" alt="2" class="scelta" onclick="change_avatar(this);">
+          <img src="../imgs/avatars/3.png" alt="3" class="scelta" onclick="change_avatar(this);">
+          <img src="../imgs/avatars/4.png" alt="4" class="scelta" onclick="change_avatar(this);">
+          <img src="../imgs/avatars/5.png" alt="5" class="scelta" onclick="change_avatar(this);">
+          <img src="../imgs/avatars/6.png" alt="6" class="scelta" onclick="change_avatar(this);">
+          <br><input type="submit" onclick="update_avatar();" value="Salva" style="width:70px;padding:10px;">
+        </div>
+
     </div></div>
 
 <script src="../main/main.js"></script>
+<script src="../main/animazioni.js"></script>
+<script src="account.js"></script>
 </body>
 </html>
---
+++ account/cambioavatar.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include "../connection.php";
+$id_utente = $_SESSION['id_utente'];
+$id_avatar = $_GET['id_avatar'];
+
+$sql = "UPDATE utenti SET id_avatar = '$id_avatar' WHERE id = '$id_utente'";
+$results = mysqli_query($conn, $sql);
+
+if ($results) {
+  echo "Modiche avvenute con successo.";
+}else {
+  echo "Qualcosa è andato storto.";
+}
+
+?>
