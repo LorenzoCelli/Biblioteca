4d3d3d722d3305c70b81dc8d3f0aee923c0b20ba
Denis <iltg407@gmail.com>
2018-07-17 09:42:20 +0200

    script avatar

--- account/scriptusericon.php
+++ account/scriptusericon.php
@@ -1,32 +1,22 @@
 <?php
 
-$sql = "SELECT * FROM utenti WHERE id = '$id_utente'";
-$results = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($results);
-$id_avatar = $row['id_avatar'];
-
-switch ($id_avatar) {
-  case 0:
-    $img = "../imgs/usericon.svg";
-    break;
-  case 1:
-    $img = "../imgs/avatars/1.png";
-    break;
-  case 2:
-    $img = "../imgs/avatars/2.png";
-    break;
-  case 3:
-    $img = "../imgs/avatars/3.png";
-    break;
-  case 4:
-    $img = "../imgs/avatars/4.png";
-    break;
-  case 5:
-    $img = "../imgs/avatars/5.png";
-    break;
-  case 6:
-    $img = "../imgs/avatars/6.png";
-    break;
+function avatar($id_avatar){
+  switch ($id_avatar) {
+    case 0:
+      return "../imgs/usericon.svg";
+    case 1:
+      return "../imgs/avatars/1.png";
+    case 2:
+      return "../imgs/avatars/2.png";
+    case 3:
+      return "../imgs/avatars/3.png";
+    case 4:
+      return "../imgs/avatars/4.png";
+    case 5:
+      return "../imgs/avatars/5.png";
+    case 6:
+      return "../imgs/avatars/6.png";
+  }
 }
 
 ?>
