77de7b30bbbb893b5e08aea13ede2b6aefbfa935
LorenzoCelli <celli.lorenzo@yahoo.it>
2018-07-18 15:47:01 +0200

    css amici

--- amici/index.php
+++ amici/index.php
@@ -27,22 +27,24 @@ $img = avatar($id_avatar);
 -->
 
 <div id="main_container">
-  <div id="main_menu">
-    <a href="../main/main.php"><button class="main_menu_button">La mia biblioteca</button></a>
-    <a href="../librerie/librerie.php"><button class="main_menu_button">Le mie librerie</button></a>
-    <a href=""><button class="main_menu_button" disabled>I miei amici</button></a>
-    <a href=""><button class="main_menu_button">Tua sorella</button></a>
+  <div id="menu_principe">
+    <a href="/libri/"><button>La mia biblioteca</button></a>
+    <a href="/librerie/"><button>Le mie librerie</button></a>
+    <a href=""><button disabled>I miei amici</button></a>
+    <a href=""><button>Tua sorella</button></a>
   </div><!--
   --><div class="content">
   <div style="border-radius: 0 10px 10px 0; height: 50px; width:50px; position: absolute; top:20px; left: 0; background-color: #f8f8f8; display: inline-block"><img onclick="slide_main_menu()" src="../imgs/menu.svg" style="height: 50px"></div>
-  <a onclick="show_menu_account()"><div class="account_container">
-    <p2 id="nome_utente"><?php echo $uname;?></p2>
-    <div style="display: inline-block; height: 50px; width: 50px; overflow: hidden"><img id="small_icon" src="<?php echo $img; ?>" alt="icona_utente_non_trovata" style="width:50px;height:50px;"></div>
-  </div></a>
-  <div id="menu_account" style="background-color:white;">
-    <p class="account_menu_header">Il tuo account</p>
-    <a href="../account/account.php"><button class="account_button">Impostazioni</button></a>
-    <button style="border-bottom: none;border-radius:0 0 10px 10px;" class="account_button">Logout</button>
+  <a onclick="show_menu_account()">
+        <div class="scatola_account">
+            <p2 id="nome_utente"><?php echo $uname;?></p2>
+            <div style="display: inline-block; height: 50px; width: 50px; overflow: hidden"><img id="small_icon" src="<?php echo $img; ?>" alt="icona_utente_non_trovata" style="width:50px;height:50px;"></div>
+        </div>
+  </a>
+  <div id="menu_account">
+    <button class="menu_account_titolo" disabled>Il tuo account</button>
+    <a href="/account/account.php"><button>Impostazioni</button></a>
+    <button>Logout</button>
   </div>
 
   <?php
