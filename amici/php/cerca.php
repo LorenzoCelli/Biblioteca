147e742e8ff0d8837f51684db338f2375608f525
Denis <iltg407@gmail.com>
2018-07-25 17:52:13 +0200

    amici

--- amici/php/cerca.php
+++ amici/php/cerca.php
@@ -8,10 +8,10 @@ for ($i=0; $i < strlen($ris_cerca); $i++) {
   $ricerca .= $ris_cerca{$i}."%";
 }
 
-$sql = "SELECT * FROM utenti
-WHERE MATCH (username) AGAINST ('$ris_cerca' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)
-UNION
-SELECT * FROM utenti WHERE username LIKE '$ricerca'";
+$sql = "SELECT * from utenti
+LEFT JOIN amici ON (amici.id_utente = $id_utente AND amici.id_amico = utenti.id)
+OR (amici.id_utente = utenti.id AND amici.id_amico = $id_utente)
+WHERE utenti.username LIKE '$ricerca' AND utenti.id != $id_utente";
 $results = mysqli_query($conn, $sql);
 if ($results->num_rows == 0) {
   echo "<p id='no_amici'>Nessun utente trovato!</p>";
@@ -21,14 +21,34 @@ if ($results->num_rows == 0) {
     $uname_amico = $row['username'];
     $avatar_amico = $row['id_avatar'];
     $img_avatar = avatar($avatar_amico);
-    echo "
-    <div class='scheda_utente2'>
-      <img src='$img_avatar'>
-      <p style='display:inline-block;'>$uname_amico</p><!--
-      --><div class='loading' style='display:inline-block;float:right;background-color:none;'></div><img src='/imgs/check.svg' style='display:none;' class='risp_richiesta'><!--
-      --><button id='friend_button' onclick='aggiungi_utenti($id_amico,this)'></button>
-    </div>
-    ";
+    if (is_null($row['id_amico'])) {
+      echo "
+      <div class='scheda_utente2'>
+        <img src='$img_avatar'>
+        <p style='display:inline-block;'>$uname_amico</p><!--
+        --><div class='loading' style='display:inline-block;float:right;background-color:none;'></div><img src='/imgs/check.svg' style='display:none;' class='risp_richiesta'><!--
+        --><button id='friend_button' onclick='aggiungi_utenti($id_amico,this)'></button>
+      </div>
+      ";
+    }else{
+      if ($row['accettato'] == 1) {
+        $p = "Già amici";
+      }elseif ($row['accettato'] == 0){
+        if ($row['id_amico'] == $id_utente) {
+          $p = "Già inviata";
+        }else {
+          $p = "Richiesta ricevuta";
+        }
+
+      }
+      echo "
+      <div class='scheda_utente2'>
+        <img src='$img_avatar'>
+        <p style='display:inline-block;'>$uname_amico</p><!--
+        --><p style='display:inline-block;float:right'>$p</p>
+      </div>
+      ";
+    }
   }
 }
 
